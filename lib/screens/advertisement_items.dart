4a78b67bfe4f6565a55e017c9608164dade02c4f
Nick Tsourektsidis <nick_tsourektsidis@epam.com>
2021-12-29 11:19:10 +0100

    Remove print

--- lib/screens/advertisement_items.dart
+++ lib/screens/advertisement_items.dart
@@ -11,7 +11,6 @@ class AdvertisementItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(items);
     return GridView.count(
       crossAxisCount: 4,
       children:
